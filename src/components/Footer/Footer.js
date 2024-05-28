70d71042fb26e82f952cac7952f936365af8b264
Arijit Karmakar <arijitkarmakar873@gmail.com>
2024-05-28 09:36:48 +0530

    FooterUpdate

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,62 +1,59 @@
 import React from 'react';
 import './Footer.scss';
-import logo from '../../logo.png';
+// import logo from '../../logo.png';
 import { Facebook } from 'lucide-react';
 import { Twitter } from 'lucide-react';
 import { Instagram } from 'lucide-react';
-import { Linkedin } from 'lucide-react';
-import { Phone } from 'lucide-react';
+import { Youtube } from 'lucide-react';
+
 
 function Footer() {
   return (
     <>
     <div className='footer bg-orange'>
-      <div>
-        <h3>Contact Info</h3>
+      <div className='footer-des'>
+        <h1 className='yum'>YumSecrets</h1>
+        <p>
+          We're all about good recipes, and about quality home cooking that everyone can enjoy. 
+          Whether you're looking for some healthy inspiration or learning how to cook a decadent desert, 
+          we've trustworthy guidance for all your foodie needs.
+        </p>
+        <div className='follow'>
+        <h3>Follow Us: </h3>
+        <a href='/'><Facebook className=' mx-3 my-3'/></a>
+        <a href='/'><Twitter className=' mx-3 my-3'/></a>
+        <a href='/'><Instagram className=' mx-3 my-3'/></a>
+        <a href='/'><Youtube className=' mx-3 my-3 '/></a>
+        </div>
         
-        <div className='my-4'>
-            <a href=''>9 8 7 6 5 4 3 2 1 0</a>
-        </div>
-        <div className='my-4'>
-            <a href=''>03323 24168 </a>
-        </div>
-        <div className='my-4'>
-            <a href=""> yumsecrets@gmail.com</a>    
-        </div>
-        {/* <div className='my-4'>
-            <a href="">Sonarpur Station Road, Sonarpur, Kolkata, 700150</a>
-        </div> */}
       </div>
       <div>
-        <h3>Follow Us</h3>
-        <div className='my-4'>
-            <a href=""> Twitter</a>
+        <h3>Contact Us</h3>
+        <div className='my-4'> 
+            <a href="/">98765 43210</a>
         </div>
         <div className='my-4'>
-            <a href="" > Facebook</a>
+            <a href="/" >033 428 0707</a>
         </div>
         <div className='my-4'>
-            <a href=""> Instagram </a>
+            <a href="/">yumsecrets@gmail.com</a>
         </div>
       </div>
       <div>
-        <h3>About</h3>
+        <h3>Useful Links</h3>
         <div className='my-4'>
-            <a href="">Advertise</a>
+            <a href="/">FAQ's</a>
         </div>
         <div className='my-4'>
-            <a href="">Career</a>
+            <a href="/">About Us</a>
         </div>
         <div className='my-4'>
-            <a href="">Terms & Conditions</a>
+            <a href="/">Privacy Policy</a>
         </div>
       </div>
     </div>
-    <div className='credit'>
-      {/* <a href=''><Facebook className=' mx-3'/></a>
-      <a href=''><Twitter className=' mx-3'/></a>
-      <a href=''><Instagram className=' mx-3'/></a>
-      <a href=''><Linkedin className=' mx-3'/></a> */}
+    <div className='credit py-2'>
+    
       {/* <img src={logo} alt="My Image" className='pic' /> */}
       <p>&copy; Created By <span className='yum'> Yum Secrets </span> | All Rights Reserved</p>
     </div>
