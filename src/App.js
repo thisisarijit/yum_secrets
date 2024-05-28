29d0a1c9c157d4f3c8c45546268cde878d5b559b
sumanmaji4 <sumanmaji736@gmail.com>
2024-05-28 09:12:50 +0530

    signup page in test mode

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.scss'
 // react router dom
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // pages
-import { Home, MealDetails, Error, Category } from './pages/index'
+import { Home, MealDetails, Error, Category, SigninSignup } from './pages/index'
 // components
 import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar'
@@ -15,6 +15,7 @@ function App() {
       <Sidebar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/register' element={<SigninSignup />} />
         <Route path='/meal/:id' element={<MealDetails />} />
         <Route path='/meal/category/:name' element={<Category />} />
         <Route path='*' element={<Error />} />
---
+++ src/pages/SigninSignup/SigninSignup.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SigninSignup() {
+  return <div>SigninSignup</div>
+}
+
+export default SigninSignup
